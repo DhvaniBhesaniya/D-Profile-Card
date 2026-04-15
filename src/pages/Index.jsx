b58bfebcd85b579ab3c5150cdb298c9096333301
DhvaniBhesaniya <narutogokuluffy009@gmail.com>
2026-04-15 13:01:53 +0530

    deleted

--- src/pages/Index.jsx
+++
@@ -1,5 +0,0 @@
-import ProfileCard from "@/components/ProfileCard";
-const Index = () => (<div className="flex min-h-screen items-center justify-center bg-background">
-    <ProfileCard />
-  </div>);
-export default Index;
